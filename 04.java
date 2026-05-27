339253708e719e585c8018c42ab3c530e42f8b3f
竹内みなみ <261it183a@s.internet.ac.jp>
2026-05-28 00:42:22 +0900

    Add files via upload

---
+++ 04.java
@@ -0,0 +1,33 @@
+// 標準体重との差の出力
+import java.util.Scanner;
+
+public class 04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        //身長(cm)の入力
+        double a = scan.nextDouble();
+        //体重(kg)の入力
+        double b = scan.nextDouble();
+        double n = a / 100;
+        //標準体重の計算
+        double m = n * n * 22;
+        
+        //標準体重より10kg軽いとき
+        if (b - m < -10) {
+            System.out.printf("%.1f\n",m);
+            System.out.println("Underweight");
+        }
+        //標準体重との差が10kg以下のとき
+        if (-10 <= b - m && b - m <= 10) {
+            System.out.printf("%.1f\n",m);
+            System.out.println("Normal");
+        }
+        //標準体重より10kg重いとき
+        if (10 < b - m) {
+            System.out.printf("%.1f\n",m);
+            System.out.println("Overweight");
+        }
+        
+    }
+}
