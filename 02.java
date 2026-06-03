1a6e20a6d060264ad7ed1f54971724c1e32d5014
竹内みなみ <261it183a@s.internet.ac.jp>
2026-06-03 22:59:43 +0900

    Update 02.java

--- 02.java
+++ 02.java
@@ -1,11 +1,11 @@
-// 円からドルへの変換（1ドル150円とする）
+// 円からドルへの変換（1ドル170円とする）
 import java.util.Scanner;
 
 public class 02 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        System.out.println(a / 150);
-        System.out.println(a % 150);
+        System.out.println(a / 170);
+        System.out.println(a % 170);
     }
 }
